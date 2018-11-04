645ac4dd5a9210e46e18845dc450bb4264c448d1
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2018-11-04 00:55:36 +0000

    Adding frontend login register design

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Support\Facades\Auth;
 
 class VerificationController extends Controller
 {
@@ -25,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo;
 
     /**
      * Create a new controller instance.
@@ -34,6 +35,13 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        if(Auth::check() && Auth::user()->role->id == 1)
+        {
+            $this->redirectTo = route('admin.dashboard');
+        } else
+        {
+            $this->redirectTo = route('author.dashboard');
+        }
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as' =>'admin.', 'prefix' => 'admin', 'namespace' => 'admin', 'middleware' => ['auth', 'admin']], function(){
+	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
+
+
+Route::group(['as' =>'author.', 'prefix' => 'author', 'namespace' => 'author', 'middleware' => ['auth', 'author']], function(){
+	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+	
+});
